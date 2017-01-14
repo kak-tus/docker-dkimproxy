2a5bd8f3bd4fad0d7b0ad4118ddc9ccc378f562d
Andrey Kuzmin <prepodam@mail.ru>
2017-01-14 19:55:56 +0300

    Lower CPU usage by vault server.
    We don't need to monitor data from vault.
    Back to latest consul-template. (reverted from commit a1a227b660c08da1209dc76ef8dd1e3c3cac0c94)

--- start_template.sh
+++ start_template.sh
@@ -5,8 +5,6 @@ delgroup user 2>/dev/null
 addgroup -g $USER_GID user
 adduser -h /home/user -G user -D -u $USER_UID user
 
-consul-template -once -template "/root/dkimproxy.map.template:/etc/dkimproxy.map"
-
 consul-template -config /etc/dkimproxy.hcl &
 child=$!
 
